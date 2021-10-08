bc1436cc5f14df353c3608d0968e7e8db4180b4b
newhobbye <robsonhobbye@gmail.com>
2021-10-08 05:43:21 -0300

    forma de inserir dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -12,6 +12,35 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Connection conn = null;
+		PreparedStatement st = null;
+		
+		try {
+			conn = Banco.abreConexao();
+			
+			st = conn.prepareStatement("insert into tb_patinadores (Nome_patinador, Sobre_Nome,"
+					+ " categoria, modulo) values ( ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, "Marx");
+			st.setString(2, "da Pri");
+			st.setString(3, "Intermediario");
+			st.setInt(4, 2);
+			
+			int linhasAfetadas = st.executeUpdate();
+			
+			System.out.println("Concluido! Linhas altaredas: " + linhasAfetadas);
+			
+		}
+		catch(SQLException e) {
+			throw new BancoException(e.getMessage());
+		}
+		finally {
+			Banco.fecharStatement(st);
+			Banco.fecharConexao();
+		}
+		
+		
+		/*
+		 * 
+		 * CODIGO RESERVADO PARA CONSULTA DE INFORMAÇÕES NO BANCO!
 		Statement st = null;
 		ResultSet rs = null;
 		
@@ -32,6 +61,9 @@ public class Program {
 			Banco.fecharResultSet(rs);
 			Banco.fecharConexao();
 		}
+		
+		
+		*/
 		
 	}
 
